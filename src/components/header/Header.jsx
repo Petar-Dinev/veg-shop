58c841c2133fcaf3f969719f6d9f84efbcbd01ab
Petar-Dinev <p.dinev55@gmail.com>
2025-01-10 16:32:04 +0200

    Refactor header component to manage navigation with and no user

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export const Header = () => {
+  const { authData } = useAuthContext();
+
   return (
     <header className={styles["site-header"]}>
       <h2>Veg Shop</h2>
@@ -9,8 +12,14 @@ export const Header = () => {
         <ul className={styles["main-nav-ul"]}>
           <Link to="/">Home</Link>
           <Link to="/catalog">Catalog</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {authData.email ? (
+            <Link to="/logout">Logout</Link>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
         </ul>
       </nav>
     </header>
