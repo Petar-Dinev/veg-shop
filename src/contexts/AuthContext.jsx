ffc3dc6f653434f3491c41ca9d6f71fd5f23a198
Petar-Dinev <p.dinev55@gmail.com>
2025-01-13 11:01:14 +0200

    rework auth context and auth service

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,9 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import * as authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const contextValues = {};
+  const [authData, setAuthData] = useState({});
+  const navigate = useNavigate();
+
+  const onRegister = async (values) => {
+    try {
+      const result = await authService.register(values);
+      setAuthData(result);
+      navigate("/");
+    } catch (err) {
+      return alert(err);
+    }
+  };
+
+  const onLogin = async (values) => {
+    try {
+      const result = await authService.login(values);
+      setAuthData(result);
+      navigate("/");
+    } catch (err) {
+      return alert(err);
+    }
+  };
+
+  const contextValues = { authData, onRegister, onLogin };
 
   return (
     <AuthContext.Provider value={contextValues}>
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,13 @@
 import { get, post } from "./api";
 
 const authPaths = {
-    'register': '/users/register'
+    register: '/users/register',
+    login: '/users/login'
 }
 export async function register({ email, username, password }) {
     return await post(authPaths.register, { email, username, password })
+}
+
+export async function login({ email, password }) {
+    return await post(authPaths.login, { email, password })
 }
