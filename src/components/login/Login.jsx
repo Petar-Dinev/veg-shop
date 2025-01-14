ca1fc63dc6c8da288239cae4d5f485fbb54dbbbd
Petar-Dinev <p.dinev55@gmail.com>
2025-01-14 16:08:38 +0200

    Add functionality on Login component and some css

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
+import { useForm } from "../../hooks/useForm";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export const Login = () => {
+  const { onLogin } = useAuthContext();
+  const [values, onChangeHandler, onSubmitHandler] = useForm(
+    {
+      email: "",
+      password: "",
+    },
+    onLogin
+  );
+
   return (
     <section className={styles["login-section"]}>
       <header className={styles["login-section-header"]}>
@@ -9,13 +20,15 @@ export const Login = () => {
       </header>
 
       <section>
-        <form className={styles["login-form"]}>
+        <form className={styles["login-form"]} onSubmit={onSubmitHandler}>
           <label htmlFor="email" className={styles["form-label"]}>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="email"
+              value={values.email}
+              onChange={onChangeHandler}
               className={styles["form-input"]}
             />
           </label>
@@ -25,6 +38,8 @@ export const Login = () => {
               name="password"
               id="password"
               placeholder="password"
+              value={values.password}
+              onChange={onChangeHandler}
               className={styles["form-input"]}
             />
           </label>
